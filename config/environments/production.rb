f874870c66fc00d2d06e7b6ba12edbc586cdef58
github akari07voi <ymytxxo29@yahoo.co.jp>
2024-09-21 13:26:28 +0900

    本番環境用送信メール設定修正（外部サーバーの設定記載しgmailで送信できるように修正）

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,4 +96,12 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://secure-shelf-44470-0fc2a04afc97.herokuapp.com/', port: 3000 }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: :login
+  }
 end
